55bb41b9b2e6ae66ad1dddb2b48b7551d300b5b0
Yeliz-Akbas <yelizgulcebi@gmail.com>
2020-04-05 13:02:21 -0500

    my first commit

---
+++ src/test/java/com/weborders/pages/AbstractTestBase.java
@@ -0,0 +1,12 @@
+package com.weborders.pages;
+import com.weborders.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+public abstract class AbstractTestBase {
+    public void AbstractBasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    protected WebDriver driver=Driver.getDriver();
+    protected WebDriverWait wait=new WebDriverWait(driver,20);
+}
